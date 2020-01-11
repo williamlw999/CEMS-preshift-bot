5010fe80f221ade5a8556f7183f33ee5d49db6ab
William Liang Wang.com <williamlw999@gmail.com>
2020-01-10 21:01:06 -0500

    fixed index js race condition using async on ready; airtable shift query functional in test phase; airtable discord tag in progress

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const base = new Airtable({apiKey: auth.airtable_token}).base(auth.airtable_base
 const client = new Discord.Client();
 
 // lambda is stupid about timezones, don't try to convert timezones
-const ts = moment().isDST() ? moment().subtract(4, 'hours') : moment().subtract(5, 'hours');
+const ts = () => moment().isDST() ? moment().subtract(4, 'hours') : moment().subtract(5, 'hours');
 
 // statuses
 var status = {
@@ -40,69 +40,39 @@ function fail(){
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 // discord client ready handler
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    if (send_preshift_message()) {
-        success();
-    } else {
-        failed();
-    }
+    msg_sent = await send_preshift_message();
+    msg_sent ? success() : fail();
 });
-    
-// grab tomorrow's shifts from airtable
-// if today is 1/1/2019, tomorrow defined as 1/2 5:30 am to 1/3 5:30 am
-function get_shifts(){
-    // get today's date
 
-    // find all shifts happening tomorrow
-    //      say notification at 1/1/2019 6:30pm
-    //      we want shifts between 1/2/2019 5:30am - 1/3/2019 5:30 am
-
-    // get top 20 shifts
-    // order shifts by time desc
-    // find when last shift of "tomorrow" is (ie. day+2 5:30 am)
-    // record all shifts until first shift (ie. day+1 5:30 am)
-    //      if day+1 5:30 am is not hit, retry with top 40 shifts
-
-    // return array of shifts 
-}
-
-// send message:
-// for each shift, generate a preshift message:
-// [@crew1 @crew2 ... ] Pre-shift notification!
-// Date: [date + time]
-// Name: 
-// Location:
-
-function send_preshift_message(){
+async function send_preshift_message(){
     guild = client.guilds.find(g => g.name === "Crimson EMS")
     if (guild && guild.channels.find(ch => ch.name === 'pre-shift-reminders')){
         preshift_channel = guild.channels.find(ch => ch.name === 'pre-shift-reminders');
-        preshift_channel.send(guild.members.find(member => member.user.tag === "wlw#8168") + " I'm in!");
+        await preshift_channel.send(guild.members.find(member => member.user.tag === "wlw#8168") + " I'm in!");
         return true;
     }
     return false;
 }
 
 exports.handler = async (event) => {
-
     client.login(auth.discord_token);
     send_preshift_message()
 
     // short-poll until client.on(ready) handler completes
     while(true) {
-        console.log(completed)
+        console.log(completed, "\n")
         if(completed) {
             break;
         }
         await sleep(100);
     }
+    client.destroy()
+    status['utc_timestamp'] = moment().format();
+    status['et_timestamp'] = ts().format();
+    status['correct_Time'] = ts().hours() == 18;
 
-    client.destroy();
-
-    status['utc_timestamp'] = moment();
-    status['et_timestamp'] = ts;
-    status['correct_Time'] = ts.hours() == 18;
-
+    console.log(status, "\n")
     return status;
 };
--- tester.js
+++ tester.js
@@ -1,63 +1,178 @@
+// dependencies
+const Airtable = require('airtable');
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const moment = require('moment-timezone');
+
+// auth file
 const auth = require('./auth.json');
 
+// api const's
+const table = new Airtable({apiKey: auth.airtable_token}).base(auth.airtable_base_token);
+const client = new Discord.Client();
 
+// lambda is stupid about timezones (forces gmt), timezones must be converted manually
+const to_est = ts => ts.isDST() ? ts.subtract(4, 'hours') : ts.subtract(5, 'hours');
+const et_to_gmt = () => moment().isDST() ? "-04:00" : "-05:00"
+const ts_to_str = ts => `DATETIME_PARSE("${ts.format('MM-DD-YYYY HH:mm')} ${et_to_gmt()}", 'MM-DD-YYYY HH:mm Z')`;
+
+// helper function to sleep the process
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+// statuses
+var status = {
+    statusCode: 200,
+    body: JSON.stringify('No message sent'),
+};
+var completed = false;
+
+function success(){
+    status = {
+        statusCode: 200,
+        body: JSON.stringify('Message sent'),
+    };
+    completed = true;
+}
+
+function fail(){
+    status = {
+        statusCode: 200,
+        body: JSON.stringify('Message failed to send'),
+    };
+    completed = true;
+}
+
+// discord client ready handler
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    var success = send_preshift_message();
-    client.destroy();
-
-    if (success) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify('Hello from Lambda!'),
-        };
-    } else {
-        return {
-            statusCode: 100,
-            body: JSON.stringify('Failed from Lambda!'),
-        };
-    }
+    send_preshift_messages(client)
 });
+
+// process records to create message json
+function process_shift_record(record) {
+    return {
+        Shift: record.get('Shift'),
+        Date: record.get('Date'),
+        Hours: record.get('Hours'),
+        EMTs: record.get('Rider Shift Record')
+    }
+}
+
+// grab tomorrow's shifts from airtable
+// if today is 1/1/2019, tomorrow defined as [1/2 5:00 am, 1/3 5:00 am)
+// NOTE: we write down ET dates on airtable, but airtable configured to use GMT
+async function get_shifts(){
+    var shifts = [];
+    // first shift of tomorrow
+    var first_tomorrow = to_est(moment()).add(1, "day").set('hour', 4).set('minute', 59);
+    first_tomorrow = to_est(moment()).set('month', 11).set('date', 3).set('hour', 4).set('minute', 59).set('year', 2019);
+    // last shift of tomorrow
+    var last_tomorrow = to_est(moment()).add(2, "day").set('hour', 5).set('minute', 0);
+    last_tomorrow = to_est(moment()).set('month', 11).set('date', 4).set('hour', 5).set('minute', 0).set('year', 2019);
     
+    // compose date range formula (IS_AFTER and IS_EFORE are not inclusive)
+    var after_first = `IS_AFTER({DATE}, ${ts_to_str(first_tomorrow)})`;
+    var before_last = `IS_BEFORE({DATE}, ${ts_to_str(last_tomorrow)})`;
+    var date_range = `IF(AND(${after_first}, ${before_last}), 1, 0)`;
 
-function send_preshift_message(){
+    console.log(date_range)
+
+    // get shifts within date_range
+    await table('Shift Tracker').select({
+        filterByFormula: date_range,
+        fields: ["Date", "Shift", "Shift Type", "Hours", "Rider Shift Record"],
+        view: "Grid view",
+        sort: [{field: "Date", direction: "asc"}, {field: "Hours", direction: "desc"}]
+    }).eachPage(function parse_shift_records(records, fetchNextPage) {
+        // `parse_shift_records` will get called for each page of records.
+        console.log("pages fetched");
+        records.forEach(function(record) {
+            shifts.push(process_shift_record(record));
+        });
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `parse_shift_records` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+    // }, function done(err) {
+    //     if (err) { console.error(err); } return;
+    });
+    return shifts;
+}
+
+// grab crew discord tags
+async function get_discord_tags(shifts) {
+    // get all user ids
+    var user_ids = new Set([]);
+    shifts.forEach(shift => shift["EMTs"].forEach(uid => user_ids.add(uid)));
+    var users_string = Array.from(user_ids).toString();
+    console.log(users_string);
+
+    var uid_to_dtag = {};
+
+    user_filter = `SEARCH(RECORD_ID(), "${users_string}") != ""`;
+    console.log(user_filter)
+    await table('Master Roster').select({
+        filterByFormula: user_filter,
+        fields: ["Discord Tag"]
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function(record) {
+            console.log('Retrieved', record.get('Name'));
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    });
+    // .eachPage(function parse_dtag_records(records, fetchNextPage) {
+    //     // `parse_dtag_records` will get called for each page of records.
+    //     console.log("pages fetched");
+    //     // records.forEach(record => uid_to_dtag[record.getId()] = record["Discord Tag"]);
+    //     // To fetch the next page of records, call `fetchNextPage`.
+    //     // If there are more records, `parse_dtag_records` will get called again.
+    //     // If there are no more records, `done` will get called.
+    //     // Note: `done` was removed due to not needing it, check airtable api for `done`
+    //     fetchNextPage();
+    // });
+    console.log(uid_to_dtag)
+    return uid_to_dtag
+}
+
+// generate the messages
+function generate_preshift_messages(guild, shifts, discord_tags) {
+
+}
+
+// send message:
+// for each shift, generate a preshift message:
+// [@crew1 @crew2 ... ] Pre-shift notification!
+// Date: [date + time]
+// Name: 
+// Location:
+
+function send_preshift_messages(client){
     guild = client.guilds.find(g => g.name === "Crimson EMS")
-    if (!guild){
-            return false;
+    if (guild && guild.channels.find(ch => ch.name === 'pre-shift-reminders')){
+        preshift_channel = guild.channels.find(ch => ch.name === 'pre-shift-reminders');
+        preshift_channel.send(guild.members.find(member => member.user.tag === "wlw#8168") + " I'm in!");
+        return true;
     }
-
-    preshift_channel = guild.channels.find(ch => ch.name === 'pre-shift-reminders')
-    if (!preshift_channel){
-        return false;
-    }
-
-    preshift_channel.send(guild.members.find(member => member.user.tag === "wlw#8168") + " I'm in!");
-    return true;
+    return false;
 }
 
-function delay(t, val) {
-   return new Promise(function(resolve) {
-       setTimeout(function() {
-           resolve(val);
-       }, t);
-   });
-}
-
-function fake_timeout(r){
-    return setTimeout(r, 1000)
-}
-
+// timeout tester
 var tester = false;
 setTimeout(() => tester = true, 5000);
-
 
 async function test() {
     console.log("hello\n");
     const sleep = ms => new Promise(r => setTimeout(r, ms));
     while(true) {
-        console.log(tester)
+        console.log(tester);
         if(tester) {
             break;
         }
@@ -69,5 +184,15 @@ async function test() {
     console.log("hello again\n");
 }
 
+async function test_runner() {
+    shifts = await get_shifts();
+    console.log(shifts)
+    uid_to_dtag = await get_discord_tags(shifts);
+
+}
+
+test_runner();
+client.login(auth.discord_token);
 test();
-client.login(auth.token);
+client.destroy();
+console.log(status)
