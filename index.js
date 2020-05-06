eb7d53a67effaa8baf9d2bdda0d01888bbf46803
William Liang Wang.com <williamlw999@gmail.com>
2020-05-06 18:40:59 -0400

    refactor: change status updates, reorganize code, modify async handlers

--- index.js
+++ index.js
@@ -27,85 +27,70 @@ const ts_to_str = ts => `DATETIME_PARSE("${ts.format('MM-DD-YYYY HH:mm')}$", 'MM
 // helper function to sleep the process
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-// error handler callback
-const err_handle = err => { console.error(err); fail(); return; };
+// error handling
+const err_handle = (err) => { console.error(err); return; };
 
-// statuses
-var status = {
-    statusCode: 200,
-    body: JSON.stringify('No message sent'),
-};
-var completed = false;
+var guild = null;
+var preshift_channel = null;
 
-function success(){
-    status['body'] =JSON.stringify('Message sent');
-    completed = true;
-}
-
-function fail(){
-    status['body'] = JSON.stringify('Message failed to send');
-    completed = true;
-}
-
-var guild;
-var preshift_channel;
 
 // discord client ready handler
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!\n`);
 
     // find our server and pre shift channel
-    guild = client.guilds.find(g => g.name === "CrimsonEMS");
-    if (guild && guild.channels.find(ch => ch.name === 'pre-shift-reminders')){
-        preshift_channel = guild.channels.find(ch => ch.name === 'pre-shift-reminders');
+    guild = client.guilds.cache.find(g => g.name === "CrimsonEMS");
+    if (guild && guild.channels.cache.find(ch => ch.name === 'pre-shift-reminders')){
+        preshift_channel = guild.channels.cache.find(ch => ch.name === 'pre-shift-reminders');
     } else {
         throw `Crimson EMS server or pre-shift-reminders channel not found`;
     }
-
-    // get shift info, emt info, and send messages
-    var shifts = await get_shifts().catch(err_handle);
-    await send_preshift_messages(shifts).catch(err_handle);
 });
 
-// reaplces linked_field with list fields from linked_table
-function retrieve_link_data(record, linked_field, linked_table, fields, flatten=false) {
-    if (!fields) throw "must specify which fields from linked_field to retrive"
-    const t = base.table(linked_table);
-    const linkage = record.fields[linked_field];
-    const linked_promises = (!linkage) ? [] : linkage.map((foreign_record_id) => {
-        return t.find(foreign_record_id);
-    });
 
-    return Promise.all(linked_promises).then((values) => {
-        values = (!values) ? [] : values.map((v) => {
-            return fields.map(field => v._rawJson["fields"][field]);
-        });
-        values = flatten ? values.flat(Infinity) : values
+// AWS Lambda trigger handler
+exports.handler = async (event) => {
+    // initial status
+    var status = {
+        utc_timestamp : moment().format(),
+        et_timestamp : moment.tz("America/New_York").format(),
+        correct_Time : moment.tz("America/New_York").hours() == 18,
+        live_mode : live_mode,
+        time_check : time_check,
+        send_msgs : send_msgs,
+    };
 
-        record.fields[linked_field] = values;
-        record._rawJson.fields[linked_field] = values;
-    }).catch(err_handle);
-}
-
-// process records to create message json
-async function process_shift_record(record) {
-    await retrieve_link_data(record, 'Rider Shift Record', 'Rider Shift Record', ["EMT"], true);
-    await retrieve_link_data(record, 'Rider Shift Record', 'Master Roster', ["Name", "Discord Tag"])
-    console.log(record.get('Rider Shift Record'))
-
-    return {
-        Shift: record.get('Shift'),
-        Date: record.get('Date'),
-        Hours: record.get('Hours'),
-        Location: record.get('Location'),
-        EMTs: record.get('Rider Shift Record')
+    // Since there are two triggers one for EST and another for EDT
+    if (time_check && !(moment.tz("America/New_York").hours() == 18)) {
+        console.log(status, "\n");
+        return status;
     }
-}
+
+    // deploy the bot
+    client.login(auth.discord_token);    
+    // short-poll until client.on(ready) handler completes
+    while(guild === null) {
+        await sleep(100);
+    }
+
+    // get shift info, emt info, and send messages
+    var shifts = await get_shifts(status).catch(err_handle);
+    status["shifts"] = shifts
+    var messages = await send_preshift_messages(shifts).catch(err_handle);
+    status["messages"] = messages;
+
+    // destroy the bot
+    await client.destroy()
+
+    console.log(status, "\n")
+    return status;
+};
+
 
 // grab tomorrow's shifts from airtable
 // if today is 1/1/2019, tomorrow defined as [1/2 5:00 am, 1/3 5:00 am)
 // NOTE: we write down ET dates on airtable, but airtable configured to use GMT
-async function get_shifts(){
+async function get_shifts(status){
     var shifts = [];
     var shift_promises = [];
     // currently airtable uses GMT times, so we do not need to convert times as lambda system is in GMT
@@ -153,16 +138,43 @@ async function get_shifts(){
     return shifts;
 }
 
-// emt record should be of the form [emt name, discord tag]
-function get_discord_tag(emt_record, guild_members) {
-    const get_member = (dtag) => guild_members.find(member => member.user.tag.toLowerCase() === dtag.toLowerCase());
-    const get_name = (name) => `${name.substr(name.indexOf(",")+2)} ${name.substr(0, name.indexOf(","))}`;
-    if (emt_record[1]) {
-        dtag = get_member(emt_record[1]);
-        return dtag ? dtag : get_name(emt_record[0])
+
+// process records to create message json
+async function process_shift_record(record) {
+    await retrieve_link_data(record, 'Rider Shift Record', 'Rider Shift Record', ["EMT"], true);
+    await retrieve_link_data(record, 'Rider Shift Record', 'Master Roster', ["Name", "Discord Tag"])
+    console.log(record.get('Rider Shift Record'))
+
+    return {
+        Shift: record.get('Shift'),
+        Date: record.get('Date'),
+        Hours: record.get('Hours'),
+        Location: record.get('Location'),
+        EMTs: record.get('Rider Shift Record')
     }
-    return get_name(emt_record[0]);
 }
+
+
+// reaplces linked_field with list fields from linked_table
+function retrieve_link_data(record, linked_field, linked_table, fields, flatten=false) {
+    if (!fields) throw "must specify which fields from linked_field to retrive"
+    const t = base.table(linked_table);
+    const linkage = record.fields[linked_field];
+    const linked_promises = (!linkage) ? [] : linkage.map((foreign_record_id) => {
+        return t.find(foreign_record_id);
+    });
+
+    return Promise.all(linked_promises).then((values) => {
+        values = (!values) ? [] : values.map((v) => {
+            return fields.map(field => v._rawJson["fields"][field]);
+        });
+        values = flatten ? values.flat(Infinity) : values
+
+        record.fields[linked_field] = values;
+        record._rawJson.fields[linked_field] = values;
+    }).catch(err_handle);
+}
+
 
 // generate the messages
 // only to be called after client is ready (through client.on(ready))
@@ -172,8 +184,7 @@ async function send_preshift_messages(shifts) {
         if (send_msgs) {
             await preshift_channel.send(no_shifts + "\n" + contact).catch(err_handle);
         }
-        success();
-        return;
+        return messages;
     }
     // generate messages
     var messages = [];
@@ -196,42 +207,20 @@ async function send_preshift_messages(shifts) {
     console.log("messages generated");
 
     messages.push(disclaimer + "\n" + contact);
-    status["messages"] = messages;
 
     var message_promises = messages.map(message => send_msgs ? preshift_channel.send(message) : undefined);
     await Promise.all(message_promises).catch(err_handle);
-    success();
-    return;
+    return messages;
 }
 
-// in case of failure, switch completed to true
-process.on('exit', () => completed = true);
 
-// AWS Lambda trigger handler
-exports.handler = async (event) => {
-    // store time stamp info 
-    status['utc_timestamp'] = moment().format();
-    status['et_timestamp'] = moment.tz("America/New_York").format();
-    status['correct_Time'] = moment.tz("America/New_York").hours() == 18;
-
-    // Since there are two triggers one for EST and another for EDT
-    if (time_check && !(moment.tz("America/New_York").hours() == 18)) {
-        console.log(status, "\n");
-        return status;
+// emt record should be of the form [emt name, discord tag]
+function get_discord_tag(emt_record, guild_members) {
+    const get_member = (dtag) => guild_members.cache.find(member => member.user.tag.toLowerCase() === dtag.toLowerCase());
+    const get_name = (name) => `${name.substr(name.indexOf(",")+2)} ${name.substr(0, name.indexOf(","))}`;
+    if (emt_record[1]) {
+        dtag = get_member(emt_record[1]);
+        return dtag ? dtag : get_name(emt_record[0])
     }
-
-    // deploy the bot
-    client.login(auth.discord_token);    
-    // short-poll until client.on(ready) handler completes
-    while(true) {
-        if(completed) {
-            break;
-        }
-        await sleep(100);
-    }
-    // destroy the bot
-    await client.destroy().catch(err_handle)
-
-    console.log(status, "\n")
-    return status;
-};
+    return get_name(emt_record[0]);
+}
